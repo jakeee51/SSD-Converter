52126851036013b1481b292e51b8aa0052189021
jakeee51 <jakemorfe@gmail.com>
2019-07-29 18:36:14 -0400

    Alpha 0.0.1

--- seven_seg.py
+++ seven_seg.py
@@ -1,15 +1,27 @@
 def seven_seg(num):
-    digit_dict = {'0': [" _ ","| |","|_|"], '1': ["   ","  |","  |"],
-                  '2': [" _ "," _|","|_ "], '3': [" _ "," _|"," _|"],
-                  '4': ["   ","|_|","  |"], '5': []}
-    SSD = []
-    layer1 = []
-    layer2 = []
-    layer3 = []
-    for d in num:
-        if d == '0':
-            SSD.append(" _ \n| | \n|_| ")
-
+    num = str(num)
+    if not num.isdigit():
+        raise TypeError("Not a number")
+    def switch(i):
+        digit_dict = {'0': [" _ ","| |","|_|"], '1': ["   ","  |","  |"],
+                      '2': [" _ "," _|","|_ "], '3': [" _ "," _|"," _|"],
+                      '4': ["   ","|_|","  |"], '5': [" _ ","|_ "," _|"],
+                      '6': [" _ ","|_ ","|_|"], '7': [" _ ","  |","  |"],
+                      '8': [" _ ","|_|","|_|"], '9': [" _ ","|_|","  |"]}
+        return digit_dict.get(i)
+    SSD = ""
+    for d in range(len(num)): #First pass
+        layer1 = switch(num[d])[0]
+        SSD += layer1
+    SSD += '\n'
+    for d in range(len(num)): #Second pass
+        layer2 = switch(num[d])[1]
+        SSD += layer2
+    SSD += '\n'
+    for d in range(len(num)): #Third pass
+        layer3 = switch(num[d])[2]
+        SSD += layer3
+    SSD += '\n'
     return " _  _ \n _| _|\n|_  _|\n"
 
 print(seven_seg('0'))
